582bb3dcce769c756ce22875bd477899b9a677be
Dan <danjacobs@gmail.com>
2023-10-16 14:25:17 +0100

    vite fix (reverted back to individual file structure)

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,28 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                '/resources/js/*.js',
-                '/resources/css/*.css'
+                'resources/css/global.scss', /* imports modal.css */
+                'resources/css/web.scss',
+                'resources/css/timeline.scss',
+                'resources/css/map.scss',
+                'resources/css/comments.scss',
+                'resources/css/filters.scss',
+                'resources/css/portal.scss',
+                'resources/css/portal/edit.scss',
+                'resources/css/resource/form.scss',
+                'resources/css/plugin/tagify.css',
+                'resources/js/global.js', /* imports jquery & jquery-modal */
+                'resources/js/web/scripts.js',
+                'resources/js/timeline/scripts.js',
+                'resources/js/timeline/map.js', /* imports jquery-resizable-dom */
+                'resources/js/timeline/comments.js',
+                'resources/js/timeline/filters.js',
+                'resources/js/portal/scripts.js',
+                'resources/js/portal/timeline/edit.js',
+                'resources/js/portal/timeline/settings.js',
+                'resources/js/portal/timeline/privacy.js',
+                'resources/js/portal/timeline/privacy-share.js',
+                'resources/js/portal/timeline/event/create.js',
             ],
             refresh: true,
         }),
