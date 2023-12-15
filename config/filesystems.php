1b01779449bca8cd0af98c67365711e852962c15
Dan <danjacobs@gmail.com>
2023-12-15 17:01:09 +0000

    revert

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('uploads'),
-            'url' => env('APP_URL').'/uploads',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
