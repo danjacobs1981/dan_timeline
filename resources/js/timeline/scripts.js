75ef79ded8272a5c4c6023fea1bad37ab44dbfca
Dan <danjacobs@gmail.com>
2024-01-25 12:05:49 +0000

    js start fix

--- resources/js/timeline/scripts.js
+++ resources/js/timeline/scripts.js
@@ -5,21 +5,130 @@ import ScrollMagic from 'scrollmagic';
 var topHeight = getTopHeight();
 
 export function start() {
+
     setLayout();
     classEvents();
     loadEvents(null, []);
+
+    $(window).on('resize', function() {
+        topHeight = getTopHeight();
+        setLayout();
+        setEventElements();
+        classEvents();
+    });
+
+    $(window).on('load', function() {
+        setTimeout(scrollOnPageLoad(), 500);
+    });
+
+    /* timeline scroll increment up/down */
+    $('i.events-increment').on('click', function() {
+        var scrollTop = 0;
+        if ($(this).hasClass('events-up') || ($(this).hasClass('events-down') && !$('.event-title').hasClass('event-last'))) {
+            var increment = -1;
+            if ($(this).hasClass('events-down')) {
+                increment = 1;
+            }
+            var order = 0;
+            if ($('.event-title.active').length) {
+                order = $('.event-title.active').data('order');
+            }
+            var $goElement = $('.event-title[data-order="' + (order + increment) + '"]');
+            if ($goElement.length) {
+                var diff = topHeight + 85;
+                if (screenSize > 2) {
+                    diff = 85;
+                }
+                scrollTop = $goElement[0].offsetTop - diff;
+            } else {
+                if ($(this).hasClass('events-down')) {
+                    //scrollTop = $('.event-first').offset().top - topHeight;
+                }
+            }
+            var el = 'html, body';
+            if (screenSize > 2) {
+                el = 'article';
+            }
+            $(el).stop(true).animate({
+                scrollTop: scrollTop
+            }, 500);
+        }
+        return false;
+    });
+
+    /* header buttons */
+    $('.header__options-share').on('click', function() {
+        $('#modal-share').modal({
+            modalClass: 'modal-share'
+        });
+    });
+
+    /* header dropdown (tag) filter */
+    $('.dropdown-toggle input[type="checkbox"]').on('change', function() {
+        var $dropdown = $(this).closest('.dropdown-toggle');
+        var dd_checked = $dropdown.find('input:checkbox:checked').length;
+        if (dd_checked > 0) {
+            $dropdown.find('em.count').remove();
+            $dropdown.find('i.fa-chevron-down').before('<em class="count">' + dd_checked + '</em>');
+        } else {
+            $dropdown.find('em.count').remove();
+        }
+        loadEvents(null, true);
+    });
+
+    /* events */
+    $('.events-wrapper').on('click', '.event-close, .event-read, .event-source, .event-subheader > li, .event img', function() {
+        var $event = $(this).closest('.event');
+        if ($event.hasClass('event--open')) {
+            $event.removeClass('event--open');
+        } else {
+            $event.addClass('event--open');
+        }
+    });
+
+    /* like */
+    $('li.header__options-like, li.header__options-save').on('click', function() {
+        var $el = $(this);
+        $el.addClass('loading');
+        var type = 'like';
+        if ($el.hasClass('header__options-save')) {
+            type = 'save';
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/timeline/' + $('meta[name="timeline"]').attr('content') + '/' + type,
+            dataType: 'json',
+        }).done(function(response) {
+            window.setTimeout(function() {
+                $el.removeClass('loading');
+                if (response.success) {
+                    if (response.increment) {
+                        if (type == 'like') {
+                            $el.addClass('color-liked').removeClass('color-like').find('span').html('Liked <em>' + response.count + '</em>');
+                        } else {
+                            $el.addClass('color-saved').removeClass('color-save').find('span').text('Saved');
+                        }
+                    } else {
+                        if (type == 'like') {
+                            $el.addClass('color-like').removeClass('color-liked').find('span').html('Like <em>' + response.count + '</em>');
+                        } else {
+                            $el.addClass('color-save').removeClass('color-saved').find('span').text('Save');
+                        }
+                    }
+                } else {
+                    $('#modal-signup').modal({
+                        modalClass: 'modal-login modal-md'
+                    });
+                }
+            }, 200);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.responseText);
+        }).always(function() {
+            // Always run after .done() or .fail()
+        });
+    });
+
 }
-
-$(window).on('resize', function() {
-    topHeight = getTopHeight();
-    setLayout();
-    setEventElements();
-    classEvents();
-});
-
-$(window).on('load', function() {
-    setTimeout(scrollOnPageLoad(), 500);
-});
 
 function getTopHeight() {
     if (screenSize > 2) {
@@ -117,10 +226,9 @@ function scrollOnPageLoad() {
     }
 }
 
-var controller = new ScrollMagic.Controller();
-
 /* timeline scroll indicator */
 function scrollEvents() {
+    var controller = new ScrollMagic.Controller();
     $('.events-wrapper .event-title').each(function() {
         new ScrollMagic.Scene({
                 offset: (-topHeight) - 86,
@@ -161,110 +269,3 @@ function scrollEvents() {
             .addTo(controller);
     });
 }
-
-/* timeline scroll increment up/down */
-$('i.events-increment').on('click', function() {
-    var scrollTop = 0;
-    if ($(this).hasClass('events-up') || ($(this).hasClass('events-down') && !$('.event-title').hasClass('event-last'))) {
-        var increment = -1;
-        if ($(this).hasClass('events-down')) {
-            increment = 1;
-        }
-        var order = 0;
-        if ($('.event-title.active').length) {
-            order = $('.event-title.active').data('order');
-        }
-        var $goElement = $('.event-title[data-order="' + (order + increment) + '"]');
-        if ($goElement.length) {
-            var diff = topHeight + 85;
-            if (screenSize > 2) {
-                diff = 85;
-            }
-            scrollTop = $goElement[0].offsetTop - diff;
-        } else {
-            if ($(this).hasClass('events-down')) {
-                //scrollTop = $('.event-first').offset().top - topHeight;
-            }
-        }
-        var el = 'html, body';
-        if (screenSize > 2) {
-            el = 'article';
-        }
-        $(el).stop(true).animate({
-            scrollTop: scrollTop
-        }, 500);
-    }
-    return false;
-});
-
-/* header buttons */
-$('.header__options-share').on('click', function() {
-    $('#modal-share').modal({
-        modalClass: 'modal-share'
-    });
-});
-
-/* header dropdown (tag) filter */
-$('.dropdown-toggle input[type="checkbox"]').on('change', function() {
-    var $dropdown = $(this).closest('.dropdown-toggle');
-    var dd_checked = $dropdown.find('input:checkbox:checked').length;
-    if (dd_checked > 0) {
-        $dropdown.find('em.count').remove();
-        $dropdown.find('i.fa-chevron-down').before('<em class="count">' + dd_checked + '</em>');
-    } else {
-        $dropdown.find('em.count').remove();
-    }
-    loadEvents(null, true);
-});
-
-/* events */
-$('.events-wrapper').on('click', '.event-close, .event-read, .event-source, .event-subheader > li, .event img', function() {
-    var $event = $(this).closest('.event');
-    if ($event.hasClass('event--open')) {
-        $event.removeClass('event--open');
-    } else {
-        $event.addClass('event--open');
-    }
-});
-
-/* like */
-$('li.header__options-like, li.header__options-save').on('click', function() {
-    var $el = $(this);
-    $el.addClass('loading');
-    var type = 'like';
-    if ($el.hasClass('header__options-save')) {
-        type = 'save';
-    }
-    $.ajax({
-        type: 'POST',
-        url: '/timeline/' + $('meta[name="timeline"]').attr('content') + '/' + type,
-        dataType: 'json',
-    }).done(function(response) {
-        window.setTimeout(function() {
-            $el.removeClass('loading');
-            if (response.success) {
-                if (response.increment) {
-                    if (type == 'like') {
-                        $el.addClass('color-liked').removeClass('color-like').find('span').html('Liked <em>' + response.count + '</em>');
-                    } else {
-                        $el.addClass('color-saved').removeClass('color-save').find('span').text('Saved');
-                    }
-                } else {
-                    if (type == 'like') {
-                        $el.addClass('color-like').removeClass('color-liked').find('span').html('Like <em>' + response.count + '</em>');
-                    } else {
-                        $el.addClass('color-save').removeClass('color-saved').find('span').text('Save');
-                    }
-                }
-            } else {
-                $('#modal-signup').modal({
-                    modalClass: 'modal-login modal-md'
-                });
-            }
-        }, 200);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR.responseText);
-    }).always(function() {
-        // Always run after .done() or .fail()
-    });
-});
